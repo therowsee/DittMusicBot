001b7ab00660a63779493ba4b922b01f616c5e85
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-27 01:35:54 +0700

    Update auth.py

--- DittMusic/plugins/admins/auth.py
+++ DittMusic/plugins/admins/auth.py
@@ -12,12 +12,12 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, adminlist
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import (delete_authuser, get_authuser,
+from DittMusic import app
+from DittMusik.utils.database import (delete_authuser, get_authuser,
                                        get_authuser_names,
                                        save_authuser)
-from YukkiMusic.utils.decorators import AdminActual, language
-from YukkiMusic.utils.formatters import int_to_alpha
+from DittMusik.utils.decorators import AdminActual, language
+from DittMusik.utils.formatters import int_to_alpha
 
 # Command
 AUTH_COMMAND = get_command("AUTH_COMMAND")
