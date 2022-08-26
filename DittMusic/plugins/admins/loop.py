7bbe41a69cda12aaa459679c0f6a3297e8e41c1d
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-27 01:36:43 +0700

    Update loop.py

--- DittMusic/plugins/admins/loop.py
+++ DittMusic/plugins/admins/loop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database.memorydatabase import (get_loop,
+from DittMusik import app
+from DittMusik.utils.database.memorydatabase import (get_loop,
                                                       set_loop)
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from DittMusik.utils.decorators import AdminRightsCheck
 
 # Commands
 LOOP_COMMAND = get_command("LOOP_COMMAND")
