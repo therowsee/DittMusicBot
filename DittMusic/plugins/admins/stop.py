167c1ce31229cfbf2d3ec98d3e51f0c4ba419d70
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-27 09:11:29 +0700

    Update stop.py

--- DittMusic/plugins/admins/stop.py
+++ DittMusic/plugins/admins/stop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import set_loop
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from DittMusic import app
+from DittMusic.core.call import Ditt
+from DittMusic.utils.database import set_loop
+from DittMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Yukki.stop_stream(chat_id)
+    await Ditt.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
