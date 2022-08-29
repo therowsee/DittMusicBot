7b1f228c746d12ffd0106a887f5236ac67c4f813
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-29 13:36:37 +0700

    Update seeker.py

--- DittMusic/plugins/misc/seeker.py
+++ DittMusic/plugins/misc/seeker.py
@@ -12,11 +12,11 @@ import asyncio
 from pyrogram.types import InlineKeyboardMarkup
 
 from strings import get_string
-from YukkiMusic.misc import db
-from YukkiMusic.utils.database import (get_active_chats, get_lang,
+from DittMusic.misc import db
+from DittMusic.utils.database import (get_active_chats, get_lang,
                                        is_music_playing)
-from YukkiMusic.utils.formatters import seconds_to_min
-from YukkiMusic.utils.inline import (stream_markup_timer,
+from DittMusic.utils.formatters import seconds_to_min
+from DittMusic.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
 checker = {}
