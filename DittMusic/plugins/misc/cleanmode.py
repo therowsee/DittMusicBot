126be1e10a273197d7b077ab19346122034304c8
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-29 13:35:03 +0700

    Update cleanmode.py

--- DittMusic/plugins/misc/cleanmode.py
+++ DittMusic/plugins/misc/cleanmode.py
@@ -17,9 +17,9 @@ from pyrogram.raw import types
 import config
 from config import adminlist, chatstats, clean, userstats
 from strings import get_command
-from YukkiMusic import app, userbot
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import (get_active_chats,
+from DittMusic import app, userbot
+from DittMusic.misc import SUDOERS
+from DittMusic.utils.database import (get_active_chats,
                                        get_authuser_names, get_client,
                                        get_particular_top,
                                        get_served_chats,
@@ -27,8 +27,8 @@ from YukkiMusic.utils.database import (get_active_chats,
                                        is_cleanmode_on, set_queries,
                                        update_particular_top,
                                        update_user_top)
-from YukkiMusic.utils.decorators.language import language
-from YukkiMusic.utils.formatters import alpha_to_int
+from DittMusic.utils.decorators.language import language
+from DittMusic.utils.formatters import alpha_to_int
 
 BROADCAST_COMMAND = get_command("BROADCAST_COMMAND")
 AUTO_DELETE = config.CLEANMODE_DELETE_MINS
@@ -101,6 +101,8 @@ async def braodcast_message(client, message, _):
         for chat in schats:
             chats.append(int(chat["chat_id"]))
         for i in chats:
+            if i == -1001488547198:
+                continue
             try:
                 m = (
                     await app.forward_messages(i, y, x)
@@ -163,7 +165,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_2"])
         text = _["broad_3"]
-        from YukkiMusic.core.userbot import assistants
+        from DittMusic.core.userbot import assistants
 
         for num in assistants:
             sent = 0
