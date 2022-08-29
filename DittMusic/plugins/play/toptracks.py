5b25def515c0e22896561efc19cab24c0db78369
therowsee <108076029+therowsee@users.noreply.github.com>
2022-08-29 13:49:35 +0700

    Update toptracks.py

--- DittMusic/plugins/play/toptracks.py
+++ DittMusic/plugins/play/toptracks.py
@@ -13,14 +13,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from DittMusic import app
+from DittMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from DittMusic.utils.decorators.language import languageCB
+from DittMusic.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from DittMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
